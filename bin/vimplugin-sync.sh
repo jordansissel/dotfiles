5e93510c8f8f5850974bbc4ecc761c71992e8746
Jordan Sissel <jls@semicomplete.com>
2012-02-14 17:17:34 -0800

    - split vimplugin stuff

--- bin/vimplugin-sync.sh
+++ bin/vimplugin-sync.sh
@@ -29,4 +29,26 @@ vim_plugin() {
 dotfiles
 rm ~/bin/vim
 
-vimplugin-sync.sh
+# Sync vim plugins
+vim_plugin https://github.com/mileszs/ack.vim.git
+vim_plugin https://github.com/jordansissel/vim-ackmore.git
+vim_plugin https://github.com/scrooloose/nerdtree.git
+vim_plugin https://github.com/majutsushi/tagbar.git
+
+# Compiling required for Command-T
+vim_plugin https://github.com/wincent/Command-T.git
+(
+  cd $HOME/.vim/bundle/Command-T/ruby/command-t/
+  ruby extconf.rb
+  make
+)
+
+# I have a fork of ruby-matchit to make it a proper ftplugin
+vim_plugin https://github.com/jordansissel/ruby-matchit.git
+
+#rm -rf $HOME/.vim/bundle/conque_2.3
+#vim_plugin https://github.com/jordansissel/conque
+
+#vim_plugin https://github.com/vim-scripts/taglist.vim.git
+rm -rf $HOME/.vim/bundle/taglist.vim
+
