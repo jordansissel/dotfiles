6feae225f1c68bf39dc01c06474acb90ba1f42cc
Jordan Sissel <jls@semicomplete.com>
2012-03-01 15:22:00 -0800

    - add drawit

--- bin/vimplugin-sync.sh
+++ bin/vimplugin-sync.sh
@@ -22,6 +22,7 @@ vim_plugin https://github.com/mileszs/ack.vim.git
 vim_plugin https://github.com/jordansissel/vim-ackmore.git
 vim_plugin https://github.com/scrooloose/nerdtree.git
 vim_plugin https://github.com/majutsushi/tagbar.git
+vim_plugin https://github.com/vim-scripts/DrawIt.git
 
 # Compiling required for Command-T
 vim_plugin https://github.com/wincent/Command-T.git
