ca258625d52998d3e935c9f5b749607f8f402848
Jordan Sissel <jls@semicomplete.com>
2013-07-02 16:24:37 -0700

    - new vim plugins

--- bin/vimplugin-sync.sh
+++ bin/vimplugin-sync.sh
@@ -31,40 +31,35 @@ vim_plugin() {
   esac
 }
 
+purge() {
+  [ -d $HOME/.vim/bundle/$1 ] && rm -rf $HOME/.vim/bundle/$1
+}{
+
 # Sync vim plugins
-vim_plugin https://github.com/mileszs/ack.vim.git
-vim_plugin https://github.com/jordansissel/vim-ackmore.git
 vim_plugin https://github.com/scrooloose/nerdtree.git
 vim_plugin https://github.com/majutsushi/tagbar.git
 vim_plugin https://github.com/vim-scripts/DrawIt.git
 vim_plugin https://github.com/jpalardy/vim-slime.git
 vim_plugin https://github.com/altercation/vim-colors-solarized.git
 #vim_plugin https://github.com/pydave/AsyncCommand.git
-rm -rf $HOME/.vim/bundle/AsyncCommand
 vim_plugin https://github.com/Lokaltog/vim-powerline.git
 vim_plugin https://github.com/tpope/vim-fugitive.git
 vim_plugin https://github.com/tpope/vim-unimpaired.git
 vim_plugin https://github.com/tpope/vim-dispatch.git
+vim_plugin https://github.com/Shougo/unite.vim
+#vim_plugin https://github.com/mileszs/ack.vim.git
+#vim_plugin https://github.com/jordansissel/vim-ackmore.git
 
 vim_plugin "http://vim.sourceforge.net/scripts/download_script.php?src_id=4318"
 
-
-# Compiling required for Command-T
-#vim_plugin https://github.com/wincent/Command-T.git
-#(
-  #cd $HOME/.vim/bundle/Command-T/ruby/command-t/
-  #ruby extconf.rb
-  #make
-#)
-[ -d $HOME/.vim/bundle/Command-T ] && rm -rf $HOME/.vim/bundle/Command-T
-
-vim_plugin https://github.com/sjbach/lusty.git
+purge "Command-T"
+purge "lusty"
+purge "AsyncCommand"
+purge "taglist.vim"
+purge "conque_2.3"
+purge "conque"
+purge "ack.vim"
+purge "vim-ackmore"
 
 # I have a fork of ruby-matchit to make it a proper ftplugin
 vim_plugin https://github.com/jordansissel/ruby-matchit.git
-
-#rm -rf $HOME/.vim/bundle/conque_2.3
-#vim_plugin https://github.com/jordansissel/conque
-
-#vim_plugin https://github.com/vim-scripts/taglist.vim.git
-rm -rf $HOME/.vim/bundle/taglist.vim
