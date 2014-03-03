2e6f274fcc6f3b4bf31f22e361c30984b71d8d87
Jordan Sissel <jls@semicomplete.com>
2014-03-03 12:47:06 -0800

    Add vim gitgutter

--- bin/vimplugin-sync.sh
+++ bin/vimplugin-sync.sh
@@ -49,6 +49,7 @@ vim_plugin https://github.com/tpope/vim-dispatch.git
 vim_plugin https://github.com/Shougo/unite.vim.git
 vim_plugin https://github.com/zhaocai/unite-tmux.git
 vim_plugin https://github.com/myusuf3/numbers.vim.git 
+vim_plugin https://github.com/airblade/vim-gitgutter.git
 #vim_plugin https://github.com/mileszs/ack.vim.git
 #vim_plugin https://github.com/jordansissel/vim-ackmore.git
 vim_plugin https://github.com/Shougo/vimproc.vim.git
